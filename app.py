7108c6334c6ec6c0a5fb50714fee71b7067b6974
thalis811-cloud <thalis811@gmail.com>
2026-04-12 23:27:49 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "ok"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    return {"status": "recebido"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
